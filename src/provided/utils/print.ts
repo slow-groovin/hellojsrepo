cbd32ce4c92bb68e22a15654846a2422b27d80db
slow-groovin <136044792+slow-groovin@users.noreply.github.com>
2025-09-01 12:36:39 +0800

    update

--- src/provided/utils/print.ts
+++ src/provided/utils/print.ts
@@ -1,8 +1,7 @@
-import color from 'chalk';
 import { add } from './add';
 
 export function printSum(a: number, b: number) {
   const answer = add(a, b);
 
-  return console.log(`The answer is: ${color.cyan(answer.toString())}`);
+  return console.log(`The answer is: ${(answer.toString())}`);
 }
